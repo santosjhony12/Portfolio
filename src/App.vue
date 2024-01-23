cfeca0c323bdd2a3db45f183a3cadab2eb7d48bc
santosjhony12 <santosdesouzajhony@gmail.com>
2024-01-22 21:22:45 -0300

    update link resume

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       <SubTitle :one="one" :two="two" class="subtitle" />
       <img :src="avatar" alt="" class="image">
       <div class="div-buttons">
-        <a id="cv-button" class="button" href="./assets/Jhony_FullStack_english.pdf" download="Jhony_FullStack_english.pdf">Download CV</a>
+        <a id="cv-button" class="button" href="./assets/Jhony_FullStack_english.pdf" download="Jhony_FullStack_english.pdf">Resume</a>
 
         <a href="https://wa.me/qr/KXCMHLGMXEESL1" id="contact-button" class="button" target="_blank">Get in touch</a>
     </div>
